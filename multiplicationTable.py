6cfc2b15f725f168f15c3b0b9f90063bccacab73
Frank Mwito <Mfranklyne039@gmail.com>
2024-08-24 22:51:36 +0300

    multiplication table with exception handling

--- multiplicationTable.py
+++ multiplicationTable.py
@@ -1,1 +1,14 @@
 # print multiplication table of a given number
+
+try:
+    num = int(input("Enter any number from 1 to 12:"))
+    
+    if num > 12 or num < 1:
+     print("Enter the number again")
+    else:
+        for i in range(1,13):
+             print(f"{i} * {num} = {num*i}")
+             
+except ValueError:
+    print("Invalid input! Please enter a numeric value between 1 and 12.")
+
