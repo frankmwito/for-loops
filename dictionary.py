923f53a83089c9ce30b27dad86e17e131e651c52
Frank Mwito <Mfranklyne039@gmail.com>
2024-08-25 23:17:51 +0300

    iterate over a dictionary and print each key-value pair

--- dictionary.py
+++ dictionary.py
@@ -1,1 +1,11 @@
 # iterate over a dictionary and print each key-value pair
+
+my_dict = {
+    "name" : "Frank",
+    "age" : 22,
+    "city" : "Nairobi",
+    "occupation": "Test Automation Engineer"
+}
+
+for key, value in my_dict.items():
+    print(f" key: {key}, Value: {value}")
