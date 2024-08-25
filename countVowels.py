353e0c215612f89fd2ecbfc63ecb149d5fcb863f
Frank Mwito <Mfranklyne039@gmail.com>
2024-08-25 23:04:50 +0300

    count vowels in a string

--- countVowels.py
+++ countVowels.py
@@ -1,1 +1,13 @@
 # count vowels in a string
+
+word = input("Enter any string:")
+vowels = "aeiouAEIOU"
+count = 0
+
+for letter in word:
+    if letter in vowels:
+        count += 1
+        
+        
+        
+print(count, "vowels")
