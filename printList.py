2445a9dac040bfa932429b5c9eae6f57ef4452b6
Frank Mwito <Mfranklyne039@gmail.com>
2024-08-25 22:55:37 +0300

    print number in list with its index

--- printList.py
+++ printList.py
@@ -1,1 +1,16 @@
 # print list with its index
+num = int(input("Enter the size of your list:"))
+numbers = list()
+
+try:
+ for i in range(num):
+    numbers.append(int(input(f"Enter any number {i+1}:")))
+
+except ValueError:
+    print("Invalid input. Please enter a valid number.")
+    exit() 
+
+print("your new list is:", numbers)
+
+for number in numbers:
+    print(f"Number at index {numbers.index(number)} is {number}")
